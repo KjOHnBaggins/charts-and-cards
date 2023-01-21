95af96ee5ed8d71ad2f53a4f393b7cd7b66bc6d0
KjOHnBaggins <okenshieldak47@gmail.com>
2023-01-21 21:13:07 +0630

    search countries via api

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,64 +1,36 @@
-import React, { forwardRef, useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ThemeContext } from "../../context/theme";
 import { Card, Col } from "reactstrap";
 
-const Header = ({ onSearchChange, toggleMenu }, top) => {
+const Header = ({ onSearchChange, toggleMenu }) => {
   const [{ dark }, toggleDark] = useContext(ThemeContext);
   const [search, setSearch] = useState(null);
 
-  // lets hardcode it for now
-
-  const countryArray = [
-    {
-      name: "United States",
-      value: "us",
-    },
-    {
-      name: "Great Britain",
-      value: "gb",
-    },
-    {
-      name: "France",
-      value: "fr",
-    },
-    {
-      name: "Switzerland",
-      value: "ch",
-    },
-    {
-      name: "Japan",
-      value: "jp",
-    },
-    {
-      name: "Korea",
-      value: "kr",
-    },
-    {
-      name: "Brazil",
-      value: "br",
-    },
-  ];
-
-  const loadOptions = (inputValue) => {
-    return {
-      options: countryArray.map((country) => {
+  const loadOptions = async (inputValue) => {
+    return await fetch(`https://restcountries.com/v3.1/name/${inputValue}`)
+      .then((response) => response.json())
+      .then((response) => {
         return {
-          value: country.value,
-          label: country.name,
+          options: response?.map((country) => {
+            return {
+              value: `${country.cca3}`,
+              label: `${country.name.official}`,
+            };
+          }),
         };
-      }),
-    };
+      })
+      .catch((err) => console.error(err));
   };
 
   const handleOnChange = (searchData) => {
     setSearch(searchData.value);
-    onSearchChange(searchData);
+    onSearchChange(searchData.value);
   };
 
   return (
-    <header id="page-topbar" ref={top}>
+    <header id="page-topbar">
       <Card>
         <div className="navbar-header">
           <div className="d-flex w-100 justify-content-between">
@@ -84,15 +56,11 @@ const Header = ({ onSearchChange, toggleMenu }, top) => {
             </Col>
             <div className="header-navigation">
               <button className="theme-icon" onClick={toggleDark}>
-                {/* <FontAwesomeIcon icon="fa-solid fa-sun" className="p-1" /> */}
                 <FontAwesomeIcon
                   icon="fa-solid fa-moon"
                   className="p-1 theme-icon__icon"
                 />
               </button>
-              {/* gear and user icon */}
-              {/* <FontAwesomeIcon icon="fa-solid fa-user" className="p-1" /> */}
-              {/* <FontAwesomeIcon icon="fa-solid fa-gear" className="p-1" /> */}
             </div>
           </div>
         </div>
@@ -101,4 +69,4 @@ const Header = ({ onSearchChange, toggleMenu }, top) => {
   );
 };
 
-export default forwardRef(Header);
+export default Header;

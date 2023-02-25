c2cea65f76cd3b05615c6828c524c75dae669570
KjOHnBaggins <okenshieldak47@gmail.com>
2023-02-25 17:34:59 +0630

    port

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ const server = new ApolloServer({
   resolvers,
 });
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: process.env.PORT || 4000 },
   context: async () => {
     const { cache } = server;
     return {
